9993d8dcffc9e3d084baee5c7c050fc2060206bc
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-05-26 21:43:38 -0400

    created variables

--- index.js
+++ index.js
@@ -4,6 +4,10 @@
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
 
+var principal = 200000;
+const interestRate = 0.05;
+let years = 30;
+let name = "John";
 
 
 
@@ -15,7 +19,8 @@ Create a variable called `monthlyInterestRate` and give it the value of interest
 
 Create another variable called `periods` and give it the value of years*12.
 */
-
+let monthlyInterestRate = interestRate / 12;
+let periods = years * 12;
 
 
 
